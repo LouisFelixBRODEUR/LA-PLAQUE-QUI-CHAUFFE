2fa86123d4a5b164baa50f5695b47f206916cad8
LouisFelixBRODEUR <lfbro1@ulaval.ca>
2025-02-21 12:05:14 -0500

    Transfer Function small fix in display

--- Identify_Transfert_Function.py
+++ Identify_Transfert_Function.py
@@ -5,25 +5,25 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
-# data_refroidissement_path = os.path.dirname(os.path.abspath(__file__))+"\\Echelon_-0_39V.csv"
-# df = pd.read_csv(data_refroidissement_path, encoding="ISO-8859-1")
-# column_arrays = {col: df[col].to_numpy() for col in df.columns}
-# data_time = column_arrays["Temps (s)"]
-# data_time = data_time[:456]
-# data_temp1 = column_arrays["Temp 1 (°C)"]
-# data_temp2 = column_arrays["Temp 2 (°C)"]
-# temp_3_base = np.max(column_arrays["Temp 3 (°C)"])
-# data_temp3 = column_arrays["Temp 3 (°C)"]-temp_3_base
-# data_temp3 = data_temp3[:456]
-
-data_rechaufement_path = os.path.dirname(os.path.abspath(__file__))+"\\Echelon_0_39V.csv"
-df = pd.read_csv(data_rechaufement_path, encoding="ISO-8859-1")
+data_refroidissement_path = os.path.dirname(os.path.abspath(__file__))+"\\Echelon_-0_39V.csv"
+df = pd.read_csv(data_refroidissement_path, encoding="ISO-8859-1")
 column_arrays = {col: df[col].to_numpy() for col in df.columns}
 data_time = column_arrays["Temps (s)"]
+data_time = data_time[:456]
 data_temp1 = column_arrays["Temp 1 (°C)"]
 data_temp2 = column_arrays["Temp 2 (°C)"]
-temp_3_base = np.min(column_arrays["Temp 3 (°C)"])
+temp_3_base = np.max(column_arrays["Temp 3 (°C)"])
 data_temp3 = column_arrays["Temp 3 (°C)"]-temp_3_base
+data_temp3 = data_temp3[:456]
+
+# data_rechaufement_path = os.path.dirname(os.path.abspath(__file__))+"\\Echelon_0_39V.csv"
+# df = pd.read_csv(data_rechaufement_path, encoding="ISO-8859-1")
+# column_arrays = {col: df[col].to_numpy() for col in df.columns}
+# data_time = column_arrays["Temps (s)"]
+# data_temp1 = column_arrays["Temp 1 (°C)"]
+# data_temp2 = column_arrays["Temp 2 (°C)"]
+# temp_3_base = np.min(column_arrays["Temp 3 (°C)"])
+# data_temp3 = column_arrays["Temp 3 (°C)"]-temp_3_base
 
 
 # Identification de la fonction de Transfert retard
@@ -37,7 +37,7 @@ params, covariance = curve_fit(sys_2iem_ordre_tau1tau2, data_time, data_temp3, p
 K_val = params[0]/-0.39
 a = params[1]*params[2]
 b = params[1]+params[2]
-print(f'Transfer Function is {round(K_val,10)}/{round(a,5)}s^2 + {round(b,5)}s + 1 * exp(-s{round(params[3],5)}')
+print(f'Transfer Function is {round(K_val,10)}/{round(a,5)}s^2 + {round(b,5)}s + 1 * exp(-s{round(params[3],5)})')
 # print(params[0])
 # print(params[1])
 # print(params[2])
